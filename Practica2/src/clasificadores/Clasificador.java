43b4a38e5bef5e3ec43c6c4cf32c536c7f130e13
pinwi <pinwi@debian>
2014-10-12 13:00:48 -0400

    Enunciado de la práctica 2

---
+++ Practica2/src/clasificadores/Clasificador.java
@@ -0,0 +1,158 @@
+package clasificadores;
+
+import datos.Datos;
+import java.util.ArrayList;
+import particionado.DivisionPorcentual;
+import particionado.EstrategiaParticionado;
+import particionado.Particion;
+import particionado.ValidacionCruzada;
+
+/**
+ * 
+ * Utiliza  una  estrategia  de  particionado  determinada para  llevar  a 
+ * cabo  su  algoritmo  de  clasificación,  empleando  el  conjunto  de  
+ * entrenamiento  para construir  el  modelo  y  el  conjunto  de  prueba  
+ * para  evaluarlo.
+ * 
+ * @author Adrián Lorenzo Mateo
+ * @author Andres Ruiz Carraco
+ */
+abstract public class Clasificador {
+    
+    //Métodos abstractos que se implementan en cada clasificador concreto
+
+    /**
+     *
+     * @param datosTrain
+     */
+        abstract public void entrenamiento (Datos datosTrain);
+
+    /**
+     *
+     * @param datosTest
+     * @return
+     */
+    abstract public ArrayList<Integer> clasifica (Datos datosTest);
+
+    /**
+     * 
+     * Obtiene el numero de aciertos y errores para calcular la tasa de fallo.
+     * 
+     * @param datos coleccion de datos a clasificar
+     * @param clas clasificador a utilizar
+     * @return porcentaje de acierto
+     * @see datos.Datos
+     * @see clasificadores.Clasificador
+     * @see clasificadores.ClasificadorNaiveBayes
+     */
+    public double error (Datos datos, Clasificador clas) {
+        ArrayList<Integer> prediccion = clas.clasifica(datos);
+        ArrayList<Integer> real = new ArrayList<>();
+       
+        int count = 0;
+       
+        for(double[] sample : datos.getDatos()){
+           real.add(Double.valueOf(sample[datos.getCategorias().indexOf("class")]).intValue());
+        }
+
+        for (int i = 0; i < prediccion.size(); i++){
+            if(prediccion.get(i).equals(real.get(i)))
+               count++;
+        }
+        /*Aciertos / totales*/
+        return ((double)count/(double)prediccion.size())*100;
+    }
+
+    /**
+     * 
+     * Realiza una clasificacion utilizando una estrategia de particionado determinada.
+     * 
+     * @param part particionador a utilizar
+     * @param datos coleccion de datos para entrenar/clasificar
+     * @param clas clasificacion a utilizar
+     * @param nParticion Parametro de configuracion (En validacion cruzada numero de particiones) 
+     *                                              (En division porcentual porcentaje de aprendizaje)
+     * @return 
+     */
+    public static ArrayList<Double> validacion(EstrategiaParticionado part, Datos datos, 
+        Clasificador clas, Integer nParticion) {
+        ArrayList<Double> res = new ArrayList<>();
+        ArrayList<Particion> particiones = part.crearParticiones(datos.getDatos().length, nParticion);
+        for(Particion idx : particiones){                
+            Datos train = datos.extraeDatosTrain(idx);
+            Datos test = datos.extraeDatosTest(idx);
+            clas.entrenamiento(train);
+            res.add(clas.error(test, clas));
+        }
+        return res;
+    }
+
+        /**
+     * 
+     * Main de para realizar las pruebas necesarias:
+     * USO:
+     *  Parametros disponibles:
+     *      -input file: entrada de datos
+     *      -cruzada: particionado con validacion cruzada
+     *      -partition: particionado porcentual
+     *      -laplace: utilizar suavizado de laplace
+     *      -debug: activar trazas de debug          
+     * 
+     * @param args cadena de opciones "Clasificador.jar -input file -cruzada|-partition [-laplace] [-debug]"
+     */
+    public static void main(String[] args) {
+
+        String inputFile = "input";
+        Integer particion = 5;
+
+        EstrategiaParticionado part = new DivisionPorcentual();
+        Clasificador clasificador = new ClasificadorNaiveBayes();
+
+        for (int i = 0; i < args.length; i++) {
+            
+            if (args[i].compareTo("-input") == 0) {
+                
+                inputFile = args[i + 1];
+                i++;
+            }
+            if (args[i].compareTo("-cruzada") == 0) {
+
+                part = new ValidacionCruzada();
+
+            }
+            if (args[i].compareTo("-laplace") == 0) {
+                
+                ((ClasificadorNaiveBayes) clasificador).setLAPLACE_FLAG(true);
+            }
+            if (args[i].compareTo("-particion") == 0) {
+                
+                particion = Integer.parseInt(args[i + 1]);
+                i++;
+            }
+            if (args[i].compareTo("-debug") == 0) {
+                
+                ((ClasificadorNaiveBayes) clasificador).setDEBUG_FLAG(true);
+            }
+        }
+        Datos d = Datos.cargaDeFichero(inputFile);
+
+        double totalError = 0;
+
+        for (int i = 0; i < 100; i++) {
+
+            double error = 0;
+            ArrayList<Double> resultados = Clasificador.validacion(
+                    part, d, clasificador, particion);
+
+            for (Double resultado : resultados) {
+                error += resultado;
+            }
+
+            error /= resultados.size();
+            totalError += error;
+        }
+        totalError /= 100;
+
+        System.out.println(totalError);
+    }
+}
---
+++ Practica2/src/datos/Datos.java
@@ -0,0 +1,273 @@
+package datos;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import particionado.Particion;
+
+
+/**
+ * 
+ * Almacena los conjuntos de datos.
+ * 
+ * @author Adrián Lorenzo Mateo
+ * @author Andres Ruiz Carrasco
+ */
+public class Datos {
+    
+    /**
+     *
+     */
+    public enum TiposDeAtributos {
+
+        /**
+         *
+         */
+        Continuo, 
+
+        /**
+         *
+         */
+        Nominal};
+
+    
+    private double [][]datos;
+    /* Tantos elementos en los arrays como columnas tenga el dataset*/
+    private ArrayList<TiposDeAtributos> tipoAtributos;
+    private ArrayList<String> categorias;
+    
+    /**
+     *
+     * @param numDatos
+     * @param tipos
+     * @param categorias
+     */
+    public Datos(int numDatos, ArrayList<TiposDeAtributos> tipos, List<String> categorias) 
+    {
+        tipoAtributos = tipos;
+        datos = new double[numDatos][tipos.size()];
+        this.categorias = new ArrayList<>(categorias);
+        
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<TiposDeAtributos> getTipoAtributos() {
+        return tipoAtributos;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double[][] getDatos() {
+        return datos;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<String> getCategorias() {
+        return categorias;
+    }
+    
+    
+    /**
+     * 
+     * Obtiene el conjunto de entrenamiento de una partición.
+     * 
+     * @param idx Particion.
+     * @return 
+     */
+    public Datos extraeDatosTrain(Particion idx) 
+    {
+        Datos datosTrain = new Datos(idx.getIndicesTrain().size(),this.tipoAtributos,
+            this.categorias);
+        int i = 0;
+        for(Integer j : idx.getIndicesTrain()) {
+            System.arraycopy(this.datos[j], 0, datosTrain.datos[i], 0, this.tipoAtributos.size());
+            i++;
+        }
+          
+        return datosTrain;
+    }
+    
+    /**
+     * 
+     * Obtiene el conjunto de test de una partición.
+     * 
+     * @param idx Particion.
+     * @return 
+     */
+    public Datos extraeDatosTest(Particion idx) 
+    {
+        Datos datosTest = new Datos(idx.getIndicesTest().size(),this.tipoAtributos,
+            this.categorias);
+        int i = 0;
+        for(Integer j : idx.getIndicesTest()) {
+            System.arraycopy(this.datos[j], 0, datosTest.datos[i], 0, this.tipoAtributos.size());
+            i++;
+        }
+          
+        return datosTest;
+    }
+    
+    /**
+     * 
+     * Mezcla de forma aleatoria las filas de una matriz.
+     * 
+     * @param ar matriz
+     */
+    static public void shuffleArray(double[][] ar)
+    {
+        Random rnd = new Random();
+        for (int i = ar.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            double a[] = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+    }
+    
+    /**
+     * 
+     *  Obtiene el conjunto de datos de un fichero.
+     * 
+     * @param nombreDeFichero
+     * @return 
+     */
+    public static Datos cargaDeFichero(String nombreDeFichero) {
+        
+        Datos datos = null;
+        Diccionario diccionario = Diccionario.getInstance();
+        Integer contadorAtributosNominales = 0;
+        Integer contadorClases = 0;
+        
+        try {
+            
+            ArrayList <TiposDeAtributos> tipoAtributos = new ArrayList<>();
+            
+            Scanner sc = new Scanner(new File(nombreDeFichero));
+            
+            /*Primera linea del fichero, numero de datos*/
+            int numDatos = Integer.parseInt(sc.nextLine());
+            
+            
+            /*Segunda linea, nombre de los campos*/
+            String[] categorias = sc.nextLine().split(",");
+            
+            for(int i=0; i<categorias.length; i++) {
+                
+                categorias[i] = categorias[i].toLowerCase();
+                
+            }
+             
+            /*Tercera linea, tipos de atributos: Nominal o Continuo*/
+            String data [] = sc.nextLine().split(",");
+            
+            for(String tipoAtrib : data) {
+            
+                if(tipoAtrib.compareTo(TiposDeAtributos.Continuo.toString()) 
+                        == 0)
+                    tipoAtributos.add(TiposDeAtributos.Continuo);
+                
+                else
+                    tipoAtributos.add(TiposDeAtributos.Nominal);
+            }   
+            
+            datos = new Datos(numDatos,tipoAtributos,Arrays.asList(categorias));
+            
+            
+            
+            /*Resto de filas, conjunto de datos separados por comas*/
+            for(int i=0; (i<numDatos && sc.hasNextLine()); i++) {
+            
+                String inputData [] = sc.nextLine().split(",");
+                
+                /* control de errores */
+                if(inputData.length == tipoAtributos.size()) {
+                    
+                    for(int j=0; j<inputData.length; j++) {
+                        
+                 
+                        if(tipoAtributos.get(j).equals(
+                                TiposDeAtributos.Nominal) && !datos.getCategorias().get(j).equals("class")) {
+                            
+                            if(!diccionario.getDiccionarioAtributos().containsKey(inputData[j])) {
+                                
+                                diccionario.getDiccionarioAtributos().put(inputData[j], contadorAtributosNominales);
+                            
+                                datos.datos[i][j] = contadorAtributosNominales++;
+                               
+                            }                              
+                                
+                            else
+                                datos.datos[i][j] = diccionario.getDiccionarioAtributos().get(inputData[j]);
+                            
+                        }
+                        else if(datos.getCategorias().get(j).equals("class")){
+                            
+                            if(!diccionario.getDiccionarioClases().containsKey(inputData[j])) {
+                                
+                                diccionario.getDiccionarioClases().put(inputData[j], contadorClases);
+                            
+                                datos.datos[i][j] = contadorClases++;
+                               
+                            }                              
+                                
+                            else
+                                datos.datos[i][j] = diccionario.getDiccionarioClases().get(inputData[j]);
+                        }
+                        else
+                            datos.datos[i][j] = Double.parseDouble(inputData[j]);
+                    }
+
+                }
+                
+            }
+        // desordena las filas de la matriz para evitar posibles sesgos
+        Datos.shuffleArray(datos.datos);
+      
+      
+        return datos;
+            
+        } catch (FileNotFoundException | NumberFormatException ex) {
+            Logger.getLogger(Datos.class.getName()).log(Level.SEVERE, null, ex);
+            return datos;
+        }
+    }
+
+    @Override
+    public String toString() {
+        
+        String s = "";
+        Diccionario dic = Diccionario.getInstance();
+        
+        for (double[] dato : datos) {
+            for (int j = 0; j < dato.length; j++) {
+                if(tipoAtributos.get(j).name().equals("Nominal") &&
+                        !categorias.get(j).equals("class"))
+                    
+                    s = s + Diccionario.getKeyByValue(dic.getDiccionarioAtributos(),(int) dato[j]) + ",";
+                else if(categorias.get(j).equals("class"))
+                    s = s + Diccionario.getKeyByValue(dic.getDiccionarioClases(),(int) dato[j]) + ",";
+                else
+                    s = s + dato[j] + ",";
+
+            }
+            s = s + "\n";
+        }
+
+        return s;
+    }
+}
