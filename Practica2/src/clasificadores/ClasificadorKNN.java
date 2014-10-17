fd5c769f90d7965c8a3c3aeb11a463946ee54873
temporal <temporal@localhost>
2014-10-17 19:37:22 +0200

    clase

--- Practica2/src/clasificadores/ClasificadorKNN.java
+++ Practica2/src/clasificadores/ClasificadorKNN.java
@@ -8,8 +8,10 @@ package clasificadores;
 import datos.Datos;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 
 /**
@@ -18,11 +20,51 @@ import java.util.Map;
  * @author Andres Ruiz Carrasco
  */
 public class ClasificadorKNN extends Clasificador{
+    
+    private double[] mediasAtributos;
+    private double[] desviacionesTipicasAtributos;
+    
+    
+    private class KNNelement{
+
+        public KNNelement(Double clas, Double euclideanDistance) {
+            this.clas = clas;
+            this.euclideanDistance = euclideanDistance;
+        }
+        
+        private Double clas;
+
+        public Double getClas() {
+            return clas;
+        }
+
+        public void setClas(Double clas) {
+            this.clas = clas;
+        }
+
+        public Double getEuclideanDistance() {
+            return euclideanDistance;
+        }
+
+        public void setEuclideanDistance(Double euclideanDistance) {
+            this.euclideanDistance = euclideanDistance;
+        }
+        private Double euclideanDistance;
+    }
+    
+    /*Comparador de elementos*/
+    private class KNNComparator implements Comparator<KNNelement>{
+        @Override
+        public int compare(KNNelement o1, KNNelement o2) {
+            Double res =  o2.euclideanDistance - o1.euclideanDistance;
+            return (res.intValue());
+        }
+    }
 
     
     private double datosEntrenamiento[][];
     private int kNN = 1;    
-
+    private PriorityQueue<KNNelement> heapNeigh = new PriorityQueue<>(this.kNN, new KNNComparator());
     
     
     public void setkNN(int kNN) {
@@ -36,9 +78,36 @@ public class ClasificadorKNN extends Clasificador{
     @Override
     public void entrenamiento(Datos datosTrain) {
     
+        int tamanioAtributos = datosTrain.getCategorias().size()-1;
+        
         datosEntrenamiento = datosTrain.getDatos();
-    
+        mediasAtributos = new double[tamanioAtributos];
+        desviacionesTipicasAtributos = new double[tamanioAtributos];
+        
+        for(int i=0 ;i<tamanioAtributos; i++){
+            
+            for(double[] dato : datosEntrenamiento){
+
+                mediasAtributos[i] += dato[i];  
+            }
+        }
+        for(int i=0 ;i<tamanioAtributos; i++)
+            mediasAtributos[i] /= datosTrain.getDatos().length;
+        
+        for (int i = 0; i < tamanioAtributos; i++) {
+
+            for (double[] dato : datosEntrenamiento) {
+                    
+                desviacionesTipicasAtributos[i] += Math.pow((dato[i] - mediasAtributos[i]),2); 
+            }
+        }
+        for (int i = 0; i < tamanioAtributos; i++) {
+            desviacionesTipicasAtributos[i] /= datosTrain.getDatos().length;
+        }
+       
+
     }
+
     /**
      * 
      * @param datosTest
@@ -46,25 +115,31 @@ public class ClasificadorKNN extends Clasificador{
      */
     @Override
     public ArrayList<Integer> clasifica(Datos datosTest) {
-
         ArrayList<Integer> res = new ArrayList<>();
         
         for(double instanciaTest[] : datosTest.getDatos()){
             
-            ArrayList<Double> distancias = new ArrayList<>();
-            
             for(double instanciaTrain[] : datosEntrenamiento){
-                
                 double distanciaEuclidea = 0;
                 
                 for(int i=0; i<instanciaTest.length; i++){
-                
                     distanciaEuclidea += Math.pow((instanciaTest[i] - instanciaTrain[i]),2);
-                
                 }
+                
                 distanciaEuclidea = Math.sqrt(distanciaEuclidea);
-                distancias.add(distanciaEuclidea);
+                if (heapNeigh.size() == this.kNN) {
+                    if (heapNeigh.peek().euclideanDistance > distanciaEuclidea){
+                        heapNeigh.poll();
+                        heapNeigh.offer(new KNNelement(distanciaEuclidea,instanciaTrain[datosTest.getCategorias().size()-1]));
+                    }
+                } else {
+                    heapNeigh.offer(new KNNelement(distanciaEuclidea,instanciaTrain[datosTest.getCategorias().size()-1]));
+                }                
+                //TOD
+                //distancias.add(distanciaEuclidea);
             }
+            
+            //TODO
             ArrayList<Double> distanciasAuxiliar = new ArrayList<>(distancias);
             Collections.sort(distanciasAuxiliar);
             
