b7eae9de4d41b5a1579abfaea2c7d78fdb228e85
pinwi <pinwi@debian>
2014-10-21 23:02:52 -0400

    Gráficas de resultados.

--- Practica2/bateriaResultados.sh
+++ Practica2/bateriaResultados.sh
@@ -1,16 +1,18 @@
 
 
 
-
+knn=(1 3 5 10 20 30)
 npercents=(10 20 30 40 50 60 70 80 90)
 ncrossvalfolds=(2 3 4 5 6 7 8)
 
-
-for percent in "${npercents[@]}"
+for knnvalue in "${knn[@]}" 
 do
-	java -jar dist/Practica2.jar -input wdbc.data -particion $percent >> wdbc_div_output
-done
-for folds in "${ncrossvalfolds[@]}"
-do
-	java -jar dist/Practica2.jar -input wdbc.data -cruzada -particion $folds  >> wdbc_cross_output
+	for percent in "${npercents[@]}"
+	do
+		java -jar dist/Practica2.jar -input wdbc.data -particion $percent -K $knn >> wdbc_div_output
+	done
+	for folds in "${ncrossvalfolds[@]}"
+	do
+		java -jar dist/Practica2.jar -input wdbc.data -cruzada -particion $folds -K $knn  >> wdbc_cross_output
+	done
 done
