0ce8eeaacdda15c0ced84528ca458dbeb43c4b12
pinwi <pinwi@debian>
2014-12-13 16:34:24 -0500

    Imprime la población cada época en un único fold (me dijo Castells que hiciera eso). Si vas a ejecutar pon nFolds bajito, 3 o 4 (piensa k ésto se repite n*(n-1) veces).

--- Practica4/src/practica4/OtimizacionPerceptron.java
+++ Practica4/src/practica4/OtimizacionPerceptron.java
@@ -45,6 +45,9 @@ public class OtimizacionPerceptron {
         Individuo bestOne;
         
         int nFolds = 10;
+        int epochs = 10;
+        int population = 10;
+        
         String inputFile = "input";
            
             
@@ -58,6 +61,17 @@ public class OtimizacionPerceptron {
             if (args[i].compareTo("-nFolds") == 0) {
 
                 nFolds = Integer.parseInt(args[i + 1]);
+                i++;
+            }
+            if (args[i].compareTo("-epochs") == 0) {
+
+                epochs = Integer.parseInt(args[i + 1]);
+                i++;
+            }
+            
+            if (args[i].compareTo("-population") == 0) {
+
+                population = Integer.parseInt(args[i + 1]);
                 i++;
             }
         }
@@ -85,38 +99,46 @@ public class OtimizacionPerceptron {
             
             for(int fold2 = 0; fold2 < (nFolds-1); fold2++){
                 
-                Entorno entorno = new Entorno(10, 0.01, 0.6, fitness,
+                Entorno entorno = new Entorno(population, 0.01, 0.6, fitness,
                 training, factoryPerceptron, nFolds-1);
                 entorno.setCruces(recombinaciones);
                 entorno.setMutaciones(mutaciones);
                 entorno.setSelecciones(selecciones);
                 
-                for(Individuo i : entorno.getPoblacion().getIndividuosPoblacion())
-                    System.out.print(" "+i.getScore());
-                System.out.println();
+                if(fold2 == 0){
+                    for(Individuo i : entorno.getPoblacion().getIndividuosPoblacion())
+                        System.out.print(" "+i.getScore());
+                    System.out.println();
+                }
                 
-                for(int i=0;i<3;i++){
+                for(int i=0;i<epochs;i++){
                     entorno.epoch();
+                    if(fold2 == 0){
+                        for(Individuo ind : entorno.getPoblacion().getIndividuosPoblacion())
+                            System.out.print(" "+ind.getScore());
+                        System.out.println();
+                    }
                 }
                 bestOnes.add(entorno.getKing());
             }
             
             Instances test = data.getData().testCV(nFolds, fold);
+            System.out.println("----BEST ONES----");
             for(Individuo i : bestOnes){
                 eval.evaluateModel(i.getClasificador(), test);
-                System.out.println(eval.errorRate());
+                System.out.println(1-eval.errorRate());
                 if(best>eval.errorRate()){
                     best = eval.errorRate();
                     bestOne = i;
                 }
                     
             }
-            System.out.println("-----------------");
+            System.out.println("--------BEST ONE------");
             bestOnes.clear();
             //error += 
             //error += eval.errorRate();
         }
-        System.out.println("<"+best+">");
+        System.out.println("<"+(1-best)+">");
 
     }      
 }
