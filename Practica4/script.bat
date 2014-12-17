59bed85f63b8902355e9f53810cb2ef74e4fa33e
Andres Ruiz <jarlex.hack@gmail.com>
2014-12-17 20:30:35 +0100

    Final

---
+++ Practica4/script.bat
@@ -0,0 +1,4 @@
+java -jar dist/Practica4.jar -nFolds 3 -epochs 10 -population 10 -input data/credit-g.arff > credit-g.results
+java -jar dist/Practica4.jar -nFolds 3 -epochs 10 -population 10 -input data/breast-cancer.arff > breast-cancer.results
+java -jar dist/Practica4.jar -nFolds 3 -epochs 10 -population 10 -input data/segment-test.arff > segment-test.results
+java -jar dist/Practica4.jar -nFolds 3 -epochs 10 -population 10 -input data/glass.arff > glass.results
